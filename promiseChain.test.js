593058c3dc4f848517d4c9e300987b98100b42d9
ddijk <dick@dijkrosoft.nl>
2019-12-04 13:43:09 +0100

    Add more tests

--- promiseChain.test.js
+++ promiseChain.test.js
@@ -1,12 +1,24 @@
 const promiseChain = require('./promiseChain')
 
+var f = (x) => x * x;
+var g = (y) => y + 3;
+
+const f_after_g = promiseChain.composeAsync(f, g);
+
 test('verify promise chaining', () => {
+    return f_after_g(2).then(x => expect(x).toBe(7));
+})
 
-    var f = (x) => x * x;
-    var g = (y) => y + 3;
+test('verify promise chaining', () => {
+    return f_after_g(3).then(x => expect(x).toBe(12));
+})
 
-    const f_after_g = promiseChain.composeAsync(f, g);
+const g_then_f = promiseChain.composeAsync(g,f);
 
-    var result;
-    return f_after_g(2).then(x => expect(x).toBe(7) );
+test('verify promise chaining', () => {
+    return g_then_f(2).then(x => expect(x).toBe(25));
+})
+
+test('verify promise chaining', () => {
+    return g_then_f(3).then(x => expect(x).toBe(36));
 })
